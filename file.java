52fa0a736804746ae43b4a8fbdc6bf730d892d7b
sanket <barsesanket007@gmail.com>
2024-04-08 22:27:54 +0530

    class file added

--- file.java
+++ file.java
@@ -1,6 +1,9 @@
-public static void main(String[] args) {
+public class file{
+    public static void main(String[] args) {
     
 
-    System.out.println("Hello World!");
-
+        System.out.println("Hello World!");
+    
+    }
 }
+
